489788cc4feee3cbd4a45b4ae5462a88b8bb5b6f
Tom <tom@moneret.fr>
2025-03-07 00:06:27 +0100

    page projets

--- Folio/src/app/app.routes.ts
+++ Folio/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { CVPageComponent } from './components/pages/cv-page/cv-page.component';
+import { GestionInterneComponent } from './components/pages/gestion-interne/gestion-interne.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { ProjetsComponent } from './components/pages/projets/projets.component';
 
@@ -7,4 +8,5 @@ export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'CV', component: CVPageComponent },
   { path: 'Projets', component: ProjetsComponent },
+  { path: 'GestionInterne', component: GestionInterneComponent },
 ];
